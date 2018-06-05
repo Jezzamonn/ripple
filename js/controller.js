ee262433343b729936bd97e32b0aa47d5c101a93
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-06-04 23:42:00 -0700

    Make it circular!

--- js/controller.js
+++ js/controller.js
@@ -35,11 +35,12 @@ export default class Controller {
 			for (let y = startY; y <= height / 2 + DOT_GAP; y += DOT_GAP) {
 				let iy = y / DOT_GAP;
 				let ir = Math.sqrt(ix * ix + iy * iy);
+				let angle = Math.atan2(iy, ix);
 
 				let localT = this.t + (ir) / 16;
 
-				let waveX = 1.2 * DOT_GAP * Math.cos(2 * Math.PI * localT);
-				let waveY = 1.2 * DOT_GAP * Math.sin(2 * Math.PI * localT);
+				let waveX = 1 * DOT_GAP * Math.cos(2 * Math.PI * localT + angle);
+				let waveY = 1 * DOT_GAP * Math.sin(2 * Math.PI * localT + angle);
 
 				context.beginPath();
 				context.arc(x + waveX, y + waveY, 1, 0, 2 * Math.PI);
