0a6a60e4f5d46f71d538b98660fb23d9a67e9721
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-06-05 08:56:57 -0700

    Experiment with drawing the circle's path. Eh.

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,4 @@
-const DOT_GAP = 10;
+const DOT_GAP = 15;
 
 export default class Controller {
 
@@ -35,15 +35,22 @@ export default class Controller {
 			for (let y = startY; y <= height / 2 + DOT_GAP; y += DOT_GAP) {
 				let iy = y / DOT_GAP;
 				let ir = Math.sqrt(ix * ix + iy * iy);
-				//let angle = Math.atan2(iy, ix);
+				let angle = Math.atan2(iy, ix);
 
-				let localT = this.t - (ir / 2);// + 2.5 * angle / (Math.PI);
+				let localT = this.t - (ir / 8)
 
-				let waveX = 1 * DOT_GAP * Math.cos(2 * Math.PI * localT);
-				let waveY = 1 * DOT_GAP * Math.sin(2 * Math.PI * localT);
+				let waveDist = 0.4 * DOT_GAP;
+				let waveX = waveDist * Math.cos(2 * Math.PI * localT + angle);
+				let waveY = waveDist * Math.sin(2 * Math.PI * localT + angle);
 
 				context.beginPath();
-				context.arc(x + waveX, y + waveY, 1, 0, 2 * Math.PI);
+				context.strokeStyle = '#AAA'
+				context.lineWidth = 0.5;
+				context.arc(x, y, waveDist, 0, 2 * Math.PI);
+				context.stroke();
+
+				context.beginPath();
+				context.arc(x + waveX, y + waveY, 2, 0, 2 * Math.PI);
 				context.fill();
 			}
 		}
