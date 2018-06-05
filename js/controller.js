a12299d71d714ebd8423e1cdc405273f01f862b1
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-06-04 18:52:16 -0700

    We got that dot grid going on

--- js/controller.js
+++ js/controller.js
@@ -3,11 +3,14 @@ const DOT_GAP = 10;
 export default class Controller {
 
 	constructor() {
-
+		this.t = 0;
 	}
 
 	update() {
-		// TODO: Some updating logic
+		this.t += 0.01;
+		if (this.t > 1) {
+			this.t --;
+		}
 	}
 
 	/**
@@ -18,21 +21,23 @@ export default class Controller {
 	 */
 	render(context, width, height) {
 		// TODO: Some rendering logic
-		let x = 0;
-		while (x > -width / 2) {
-			x -= DOT_GAP;
+		let startX = 0;
+		while (startX > -width / 2) {
+			startX -= DOT_GAP;
 		}
-		let y = 0;
-		while (y >= -height / 2) {
-			y -= DOT_GAP;
+		let startY = 0;
+		while (startY >= -height / 2) {
+			startY -= DOT_GAP;
 		}
 
-		for (x; x < width; x += DOT_GAP) {
-			for (y; y < height; y += DOT_GAP) {
+		for (let x = startX; x <= width / 2 + DOT_GAP; x += DOT_GAP) {
+			let waveX = DOT_GAP * Math.cos(2 * Math.PI * this.t);
+			for (let y = startY; y <= height / 2 + DOT_GAP; y += DOT_GAP) {
+				let waveY = DOT_GAP * Math.sin(2 * Math.PI * this.t);
+
 				context.beginPath();
-				// context.fillStyle = 'black';
-				context.arc(x, y, 5, 0, Math.PI);
-				context.stroke();
+				context.arc(x + waveX, y + waveY, 1, 0, 2 * Math.PI);
+				context.fill();
 			}
 		}
 	}
